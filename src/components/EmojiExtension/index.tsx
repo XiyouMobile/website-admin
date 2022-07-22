0adc8be177ac9675bc00dd9f9e2157df29d15644
huangyulie <1279072972@qq.com>
2022-07-22 20:26:49 +0800

    feat: Miroku_1.09

---
+++ src/components/EmojiExtension/index.tsx
@@ -0,0 +1,78 @@
+import { useState } from 'react';
+import MdEditor from 'md-editor-rt';
+import { emojis } from './data';
+import './index.less';
+
+const DropdownToolbar = MdEditor.DropdownToolbar;
+
+interface EmojiExtensionProp {
+  editorId: string;
+  onChange: (v: string) => void;
+}
+
+const EmojiExtension = (props: EmojiExtensionProp) => {
+  const [state, setState] = useState({
+    visible: false,
+  });
+
+  const emojiHandler = (emoji: string) => {
+    // 获取输入框
+    const textarea = document.querySelector(`#${props.editorId}-textarea`) as HTMLTextAreaElement;
+    // 获取选中的内容
+    const selection = window.getSelection()?.toString();
+    // 获取鼠标位置
+    const endPoint = textarea.selectionStart;
+
+    // 根据鼠标位置分割旧文本
+    // 前半部分
+    const prefixStr = textarea.value.substring(0, endPoint);
+    // 后半部分
+    const suffixStr = textarea.value.substring(endPoint + (selection?.length || 0));
+
+    props.onChange(`${prefixStr}${emoji}${suffixStr}`);
+
+    setTimeout(() => {
+      textarea.setSelectionRange(endPoint, endPoint + 1);
+      textarea.focus();
+    }, 0);
+  };
+
+  const onChange = (visible: boolean) => {
+    setState({
+      visible,
+    });
+  };
+
+  return (
+    <DropdownToolbar
+      title="表情"
+      visible={state.visible}
+      onChange={onChange}
+      trigger={
+        <svg className="md-icon" aria-hidden="true">
+          <use xlinkHref="#icon-github" />
+        </svg>
+      }
+      overlay={
+        <div className="emoji-container">
+          <ol className="emojis">
+            {emojis.map((emoji, index) => {
+              return (
+                <li
+                  key={`emoji-${index + 1}`}
+                  onClick={() => {
+                    emojiHandler(emoji);
+                  }}
+                >
+                  {emoji}
+                </li>
+              );
+            })}
+          </ol>
+        </div>
+      }
+    />
+  );
+};
+
+export default EmojiExtension;
